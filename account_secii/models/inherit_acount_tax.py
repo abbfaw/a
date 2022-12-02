dafda1346d50471462d636400c6aaa5349c894a3
abbfaw <112594662+abbfaw@users.noreply.github.com>
2022-12-02 09:35:34 +0000

    Delete account_secii directory

--- account_secii/models/inherit_acount_tax.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models, api
-
-
-class InheritAccountTax(models.Model):
-    _inherit = 'account.tax'
-
-    exclude_tax = fields.Boolean(string='TVA non facturée')
-    exclude_amount = fields.Float(string='coéfficient')
