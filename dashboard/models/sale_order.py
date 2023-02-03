97200714fbf23127493e03430c6486a72ad0be37
abbfaw <112594662+abbfaw@users.noreply.github.com>
2023-02-03 13:45:10 +0000

    Delete dashboard directory

--- dashboard/models/sale_order.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, api, models
-
-class SaleOrderLineInherit(models.Model):
-	_inherit = "sale.order.line"
-
-	# montant_du = fields.Float(string="Montant dû")
-	# montant_total = fields.Float(string="Montant total")
-	ratio_client = fields.Float(string="Ratio client")
