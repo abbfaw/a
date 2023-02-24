903cd951113335f8f1247bbc594d28fef549c250
abbfaw <112594662+abbfaw@users.noreply.github.com>
2023-02-24 00:15:29 +0000

    Add files via upload

---
+++ account_secii/suivi/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from . import encaissement
+from . import facture
+from . import paiement
+from . import ventes
+from . import tracking_instance_partner
+from . import charge_partner
+from . import stock_move
+from . import purchase_order
