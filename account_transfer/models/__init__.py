9b5852d45cb614e67beb102cd15a5aa3c10ca793
abbfaw <112594662+abbfaw@users.noreply.github.com>
2023-03-22 15:21:54 +0000

    Delete account_transfer directory

--- account_transfer/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import transfer
-from . import vendor_payment
-from . import balance
-from . import vendor
-from . import cashflow
-from . import gainflow
-from . import vendor_balance
