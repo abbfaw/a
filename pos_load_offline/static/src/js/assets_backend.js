94bb97d760376eb1fa0583716ce68e94bd355270
abbfaw <112594662+abbfaw@users.noreply.github.com>
2023-04-04 15:24:01 +0000

    Delete pos_load_offline directory

--- pos_load_offline/static/src/js/assets_backend.js
+++
@@ -1,7 +0,0 @@
-function remove_pos_database() {
-    localStorage.clear();
-    var database = this.odoo.session_info.db;
-    for (var i = 0; i &lt; 100; i++) {
-    IndexedDB.deleteDatabase(database + '_' + i);
-    }
-}
