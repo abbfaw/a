519e7d6adfaf8b5f5b5ba403174e22402b037182
abbfaw <112594662+abbfaw@users.noreply.github.com>
2023-05-31 18:38:05 +0000

    Delete mediano_website directory

--- mediano_website/data/neutralize.sql
+++
@@ -1,9 +0,0 @@
--- disable delivery carriers
-UPDATE product_product
-   SET prod_environment = false,
-       active = false;
-
--- disable delivery carriers
-UPDATE delivery_carrier
-   SET prod_environment = false,
-       active = false;
