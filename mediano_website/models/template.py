37527f855a1c2f0118e86cfa1c9d845aba35f026
abbfaw <112594662+abbfaw@users.noreply.github.com>
2023-06-01 10:43:07 +0000

    Delete mediano_website directory

--- mediano_website/models/template.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, api, _, fields
-
-class TemplateSms(models.Model):
-    _name = 'template.sms'
-    _description = 'SMS Templates'
-
-    name = fields.Char(string='Template Name', required=False)
-    sms_content = fields.Text(string='Sms Content', required=False)
