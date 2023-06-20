7ebdeb8b224c4c747455f47792a3b2e136660ab0
abbfaw <112594662+abbfaw@users.noreply.github.com>
2023-06-20 16:42:17 +0000

    Delete agentis_resequenser directory

--- agentis_resequenser/models/account_move.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class AccountMoveInherit(models.Model):
-    _inherit = "account.move"
-
-    def action_open_resequence_view(self):
-        return self.env['ir.actions.act_window']._for_xml_id('account.action_account_resequence')
